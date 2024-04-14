c9252863f14a61f0d9376655019df217cc1157a8
marconys <marconysbatera@gmail.com>
2024-04-13 21:50:16 -0300

    feat: created function sacar

--- contaBancaria.py
+++ contaBancaria.py
@@ -22,3 +22,31 @@ def depositar(saldo, valor, extrato, /):
 
     return saldo, extrato
 
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("\n*** Operação falhou! Você não tem saldo suficiente. ***")
+
+    elif excedeu_limite:
+        print("\n*** Operação falhou! O valor do saque excede o limite. ***")
+
+    elif excedeu_saques:
+        print("\n*** Operação falhou! Número máximo de saques excedido. ***")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
+
+    else:
+        print("\n*** Operação falhou! O valor informado é inválido. ***")
+
+    return saldo, extrato
+
+
+   
